6c5cf9b06e3e8fef50de12debfc5a9938f9f81d8
Göksel BAŞ <gokselbas1997@gmail.com>
2023-06-05 14:12:54 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>  
+<html>  
+    <head>  
+        <meta charset="utf-8">  
+        <title>First Project</title>  
+    </head>  
+    <body>  
+        <div id="container">  
+          
+            <header>  
+                <p>PHP MESAJI</p>  
+            </header>  
+              
+          <?php
+         
+          echo "Merhaba Bu Benim İlk GitHub da bulunan PHP Projem.";
+          
+         ?>
+           
+        </div>  
+    </body>  
+</html>  
